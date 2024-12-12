4e5ccda2c5298f6d59345c8e7e732b7c07e377da
JakubBlicharz <kubablicharz@icloud.com>
2024-12-12 11:20:47 +0100

    poprawki

--- app/src/main/java/pl/destroyer/notatki/Components/Language.kt
+++ app/src/main/java/pl/destroyer/notatki/Components/Language.kt
@@ -3,11 +3,13 @@ package pl.destroyer.notatki.Components
 import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.launch
 import pl.destroyer.notatki.R
@@ -22,7 +24,9 @@ fun LanguageDropdownMenu(setAppLanguage: (String) -> Unit, drawerState: DrawerSt
     var selectedLanguage by remember { mutableStateOf(sharedPreferences.getString("language", "pl")!!) }
 
     Column {
-        Button(onClick = { expanded = true }) {
+        Button(onClick = { expanded = true }, colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF452971),
+            contentColor = Color.White)) {
             Text(
                 "${context.getString(R.string.language_label)}: ${
                     if (selectedLanguage == "pl") context.getString(R.string.polish)
